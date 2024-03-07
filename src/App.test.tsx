5907429ecafc95e9700b4201327bc6c796b8e5c3
Cristian Mello <cristianc.mello@gmailcom>
2024-03-07 10:04:52 -0300

    refact: test => it em App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,13 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("Componente App", () => {
-  test("o display deve mostrar '0'", () => {
+  it("o display deve mostrar '0'", () => {
     render(<App />);
 
     expect(screen.getByTestId("display-value")).toHaveDisplayValue("0");
   });
 
-  test("o display deve mostrar botao '0'", () => {
+  it("o display deve mostrar botao '0'", () => {
     render(<App />);
 
     const zeroButton = screen.getByTestId("zero-button");
