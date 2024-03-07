e33ddd032a47507a8a5fc62883adcd38cc7a47e2
Cristian Mello <cristianc.mello@gmailcom>
2024-03-07 10:03:59 -0300

    refact: adicionando jest describe em teste de App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,16 +2,18 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-test("o display deve mostrar '0'", () => {
-  render(<App />);
+describe("Componente App", () => {
+  test("o display deve mostrar '0'", () => {
+    render(<App />);
 
-  expect(screen.getByTestId("display-value")).toHaveDisplayValue("0");
-});
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("0");
+  });
 
-test("o display deve mostrar botao '0'", () => {
-  render(<App />);
+  test("o display deve mostrar botao '0'", () => {
+    render(<App />);
 
-  const zeroButton = screen.getByTestId("zero-button");
+    const zeroButton = screen.getByTestId("zero-button");
 
-  expect(zeroButton.textContent).toBe("0");
+    expect(zeroButton.textContent).toBe("0");
+  });
 });
