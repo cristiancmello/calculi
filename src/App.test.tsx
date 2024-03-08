e59e848e47671ec1a5985987941ff9b9c07ecbbd
PabloZalem <zalem.pablo@yahoo.com>
2024-03-08 12:03:04 -0300

    Criamos os 9 botões

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import App from "./App";
+import { it } from "node:test";
 
 describe("Componente App", () => {
   // Os casos de testes foram inspirados no Apple Calculator (macOS e iOS)
@@ -19,12 +20,81 @@ describe("Componente App", () => {
    });
 
    // Aqui é o mesmo que ocorre quando o valor é 1
+   // it é uma função definida pela framework jasmine 
+   // Segundo argumento é o arrow function -> () =>{}
    it("o display deve mostrar botao '1'", () => {
+    // Render aqui ele ajuda a gente a imprimir uma informação no navegador, nesse caso é o que foi criado no App.tsx;
     render(<App />);
 
+    // Const - Constante;
+    // screen - Uma propriedade reservada do DOM (Data Objects Model)
+    // getByTestId - Query do componente -> query por sua vez nos ajuda a manipular dos parametros de URL
     const oneButton = screen.getByTestId("one-button");
+
+    // fireEvent = Ele permite que você dispare eventos DOM simples, como click, change, mouseover, etc
     fireEvent.click(oneButton); // Simulando o clique no botão 1;
+
+    // expect lhe dá acesso a inúmeros "matchers" que permitem validar diferentes coisas.
+    // screen - Propriedade reservada;
+    // getByTestId - Query do que está contido no display-value;
+    // toHaveDisplayValue - se o valor que está no display é o valor que informamos
     expect(screen.getByTestId("display-value")).toHaveDisplayValue("1");
+   });
+
+   it("O display deve mostrar o botão '2", () => {
+    render(<App />);
+    const twoButton = screen.getByTestId("two-button");
+    fireEvent.click(twoButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("2");
+   });
+
+   it("O display deve monstrar o botão '3", () => {
+    render(<App />);
+    const threeButton = screen.getByTestId("three-button");
+    fireEvent.click(threeButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("3");
+   });
+
+   it("O display deve mostrar o botão 4", () => {
+    render(<App />);
+    const fourButton = screen.getByTestId("four-button");
+    fireEvent.click(fourButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("4");
+   });
+
+   it("O display deve mostrar o botão 5", () => {
+    render(<App />);
+    const fiveButton = screen.getByTestId("five-button");
+    fireEvent.click(fiveButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("5");
+   });
+
+   it("O display deve mostrar o botão 6", () => {
+    render(<App />);
+    const sixButton = screen.getByTestId("six-button");
+    fireEvent.click(sixButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("6");
+   });
+
+   it("O display deve mostrar o botão 7", () => {
+    render(<App />);
+    const sevenButton = screen.getByTestId("seven-button");
+    fireEvent.click(sevenButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("7");
+   });
+
+   it("O display deve mosntrar o botão 8", () => {
+    render(<App />);
+    const eigthButton = screen.getByTestId("eight-button");
+    fireEvent.click(eigthButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("8");
+   });
+
+   it("O display deve mostrar o botão 9", () => {
+    render(<App />);
+    const nineButton = screen.getByTestId("nine-button");
+    fireEvent.click(nineButton);
+    expect(screen.getByTestId("display-value")).toHaveDisplayValue("9");
    });
 
   it("o display deve mostrar os botões {0, 1, 2, ..., 9}", () => {
